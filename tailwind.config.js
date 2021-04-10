c04d9482aa7e35acb8f9b45c6d7b039ae74a43bb
lbavendanos <lbavendanos@gmail.com>
2021-04-10 11:42:06 -0500

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      acian: '#6769FF',
+      gray: '#A3ABCC',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
